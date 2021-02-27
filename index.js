c97e160a7c9a798f64fe58b6476db06f33bac1a9
mpross512 <mpross512@gmail.com>
2021-02-27 14:52:53 -0600

    plant

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const dom = require('jsdom');
 //const $ = (require('jquery'))(dom.window);
 //window.jQuery = window.$ = $;
 
+var firebase = require('firebase');
+require('firebase/auth');
+require('firebase/firestore');
+const firebaseConfig = {
+    apiKey: "AIzaSyCoE1IX59LpgjMvLlmEpydUZZpc1X0Pum0",
+    authDomain: "growth-spurt-6964f.firebaseapp.com",
+    databaseURL: "https://growth-spurt-6964f-default-rtdb.firebaseio.com",
+    projectId: "growth-spurt-6964f",
+    storageBucket: "growth-spurt-6964f.appspot.com",
+    messagingSenderId: "554230997796",
+    appId: "1:554230997796:web:7fb9e3d67f79dba946c9b1",
+    measurementId: "G-QG489YRBPY"
+  };
+firebase.initializeApp(firebaseConfig);
+
 const server = http.createServer((req, res) => {
 
     //Get file path
