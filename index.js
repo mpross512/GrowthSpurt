917bd5cf5231bc8ff9f293b2916379b725464278
mpross512 <mpross512@gmail.com>
2021-02-27 12:22:40 -0600

    Updated server to work with more content

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
+
+    //Get file path
+    let filepath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filepath);
+
+    let contentType = 'text/html';
+
+    //Get correct content type
+    switch(extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+
+    //Send data to website
+    fs.readFile(filepath, (err, content) => {
+        if(err) {
+            if(err.code = 'ENOENT') {
+
+            }
+        } else {
+            res.writeHead(200, { 'content-type': contentType });
+            res.end(content);
+        }
+    })
+
+    
+    //console.log(`Loaded file ${filepath}`);
 })
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000);
