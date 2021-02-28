bf8bcc228cda9c51316fe563bdf97b35175d7565
Joshua Olowoyeye <josh.olowoyeye@gmail.com>
2021-02-28 11:40:58 -0600

    default zero

--- public/resources/scripts/plant.js
+++ public/resources/scripts/plant.js
@@ -22,8 +22,8 @@ $(function() {
                     $(".page").hide();
                     $("#navbar").show();
                     $("#user-button").text(profile.name);
-                    $("#streak").text("Leaves: " + profile.streak);
-                    $(".plantImage img").attr("src", `resources/images/${profile.streak}leaves${profile.perfectWeek ? "" : "Brown"}.png`);
+                    $("#streak").text("Leaves: " + profile.leaves);
+                    $(".plantImage img").attr("src", `resources/images/${profile.leaves}leaves${profile.perfectWeek ? "" : "Brown"}.png`);
                     $("#remaining").text(`Days until next leaf: ${(profile.perfectWeek ? 8 : 15) - (new Date().getDay() == 0 ? 7 : new Date.getDay()) }`);
                     $("#plant-page").show();
                 }
@@ -102,7 +102,8 @@ $(function() {
         var input = document.createElement("input")
         input.type='number';
         input.id = 'addition'
-        input.min = 1;
+        input.defaultValue = 0;
+        input.min = 0;
         input.max = 69;
         input.classList.add('hide');
         var confirm = document.createElement("BUTTON");
