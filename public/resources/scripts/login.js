80d15cd57dd07176ec43b16b17317dc4f62dda20
Michael Ross <mpross512@gmail.com>
2021-02-27 18:23:29 -0600

    Added names to users

--- public/resources/scripts/login.js
+++ public/resources/scripts/login.js
@@ -12,19 +12,28 @@ window.onload = function() {
     };
     
     firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
     
 
 //Get elements
-const txtEmail = document.getElementById('txtEmail')
+const loginDiv = document.getElementById('login');
+const signupDiv = document.getElementById('signup');
+const txtName = document.getElementById('txtName');
+const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
+const loginEmail = document.getElementById('loginEmail');
+const loginPassword = document.getElementById('loginPassword');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUp = document.getElementById('btnSignUp');
 const btnLogout = document.getElementById('btnLogout');
+const btnSignUpPage = document.getElementById('btnSignUpPage');
+var newUser = false;
+var name;
 
 btnLogin.addEventListener('click', e => {
     //Get email and pass
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
+    const email = loginEmail.value;
+    const pass = loginPassword.value;
     const auth = firebase.auth();
 
     //Sign in
@@ -36,21 +45,48 @@ btnLogin.addEventListener('click', e => {
 btnSignUp.addEventListener('click', e =>{
     //Get email and pass
     //TODO: Check 4 real email
+    name = txtName.value;
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
+    newUser = true;
 
     //Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-
+    var promise = auth.createUserWithEmailAndPassword(email, pass);
     promise.catch(e => console.log(e.message));
+
+    
+
+    //var promise2 = auth.signInWithEmailAndPassword(email, pass);
+    //promise2.catch(e => console.log(e.message));
+
+    
+
+});
+
+btnSignUpPage.addEventListener('click', e => {
+    loginDiv.classList.add("hide");
+    signupDiv.classList.remove("hide");
 });
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){
         console.log(firebaseUser);
-        console.log('logged in');
-        window.location.replace('index.html')
+        console.log(`logged in ${newUser}`);
+        if(newUser) {
+            db.collection("Users").doc(firebaseUser.uid).set({
+                name: name,
+                streak: 0
+            })
+            .then(() => {
+                console.log("Document successfully written!");
+                window.location.replace('index.html')
+            })
+            .catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+        } else {
+        }
     }
     else{
         console.log('not logged in');
